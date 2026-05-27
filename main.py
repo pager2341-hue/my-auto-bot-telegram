4d6cdf8bfca956a7a8aefc59b600704dd847ac49
pager2341-hue <pager2341@gmail.com>
2026-05-27 15:59:49 +0630

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from telethon import TelegramClient
 import asyncio
 import os
 
-# API အချက်အလက်များ (အတူတူပင်)
+# API အချက်အလက်များ
 API_ID = 39359433                         
 API_HASH = '34e0e459bb9dd046a0ed4db3311ea5b8'          
 
-MYANMAR_TARGETS = [-1003762199457, -1003157381562]
+# လသာသာ Channel ID ကို ဖြုတ်လိုက်ပါပြီ
+MYANMAR_TARGETS = [-1003157381562] 
 FOREIGN_TARGETS = [-1003828762835]
 MY_MYANMAR_CHANNEL = '@abodlppdi'
 MY_FOREIGN_CHANNEL = '@ninkopint'
@@ -27,17 +28,15 @@ async def main():
     await client.connect()
     history = get_history() 
 
-    # ၁။ မြန်မာကားများအတွက်
+    # ၁။ မြန်မာကားများအတွက် (လသာသာ မပါတော့ပါ)
     for target in MYANMAR_TARGETS:
         async for message in client.iter_messages(target, limit=5, filter=telethon.tl.types.InputMessagesFilterVideo()):
             if str(message.id) not in history:
                 duration = message.file.duration if message.file else 0
                 if duration >= 60:
-                    # ဗီဒီယိုမတင်ခင် ၃ မိနစ် ကြိုတင်သတိပေးခြင်း
                     await client.send_message(MY_MYANMAR_CHANNEL, "⚠️ ဗီဒီယိုအသစ်တင်တော့မှာဖြစ်လို့ စိတ်အနှောင့်အယှက်မဖြစ်စေရန် Notification များကို ခဏပိတ်ထားပေးပါခင်ဗျာ။ ကျေးဇူးတင်ပါတယ်! ❤️", silent=False)
-                    await asyncio.sleep(180) # ၃ မိနစ်စောင့်မည်
+                    await asyncio.sleep(180) 
                     
-                    # ဗီဒီယိုတင်ခြင်း
                     await client.send_file(MY_MYANMAR_CHANNEL, file=message.media, caption="🇲🇲 မြန်မာစာတန်းထိုးရုပ်ရှင်များ ❤️", silent=True)
                     save_history(message.id)
                     await asyncio.sleep(15)
@@ -48,11 +47,9 @@ async def main():
             if str(message.id) not in history:
                 duration = message.file.duration if message.file else 0
                 if duration >= 60:
-                    # ဗီဒီယိုမတင်ခင် ၃ မိနစ် ကြိုတင်သတိပေးခြင်း
                     await client.send_message(MY_FOREIGN_CHANNEL, "⚠️ ဗီဒီယိုအသစ်တင်တော့မှာဖြစ်လို့ စိတ်အနှောင့်အယှက်မဖြစ်စေရန် Notification များကို ခဏပိတ်ထားပေးပါခင်ဗျာ။ ကျေးဇူးတင်ပါတယ်! ❤️", silent=False)
-                    await asyncio.sleep(180) # ၃ မိနစ်စောင့်မည်
+                    await asyncio.sleep(180) 
                     
-                    # ဗီဒီယိုတင်ခြင်း
                     await client.send_file(MY_FOREIGN_CHANNEL, file=message.media, caption="🍿 Enjoy Your Movies! ❤️", silent=True)
                     save_history(message.id)
                     await asyncio.sleep(15)
@@ -60,4 +57,4 @@ async def main():
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-          
+                    
